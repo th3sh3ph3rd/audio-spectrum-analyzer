16611d60d0f2c0d26e8605b842a57a807e412114
Jan Nausner <jan.nausner@gmail.com>
2022-11-19 13:52:56 +0100

    wifi station mode working

---
+++ main/wifi_sta.h
@@ -0,0 +1,16 @@
+/*
+ * Author: Jan Nausner <jan.nausner@gmail.com>
+ * 
+ * Launch WiFi station on ESP32.
+ *
+ */
+
+#ifndef WIFI_STA_H
+#define WIFI_STA_H
+
+/*
+ * Initialize WiFi module in station mode.
+ */
+void wifi_init_sta(void);
+
+#endif
